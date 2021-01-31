a1323ba9b0c143f95ae4e0dbcb2dd7f1abaf1692
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2021-01-31 20:57:00 +0300

    Tuple

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+/*
+const person: {
+  name: string;
+  age: number;
+} = {
+*/
 var person = {
     name: 'Maximilian',
-    age: 30
+    age: 30,
+    hobbiew: ['Sports', 'Cooking'],
+    role: [2, 'author']
 };
+person.role.push('admin');
+// person.role[1] = 10;
+var favoriteActivities;
+favoriteActivities = ['sport'];
 console.log('%c custom log', 'color:red;', person);
+for (var _i = 0, _a = person.hobbiew; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log('%c custom log', 'color:red;', hobby.toUpperCase());
+    // console.log('%c custom log', 'color:red;', hobby.map());
+}
--- app.ts
+++ app.ts
@@ -4,9 +4,29 @@ const person: {
   age: number;
 } = {
 */
-const person = {
+const person : {
+  name: string;
+  age: number;
+  hobbiew: string[],
+  role: [number, string];
+} = {
   name: 'Maximilian',
   age: 30,
+  hobbiew: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
 
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['sport'];
+
 console.log('%c custom log', 'color:red;', person);
+
+for (const hobby of person.hobbiew) {
+  console.log('%c custom log', 'color:red;', hobby.toUpperCase());
+  // console.log('%c custom log', 'color:red;', hobby.map());
+}
