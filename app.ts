ed190cd66c4fa9823339b7fa3da1f8f5110954aa
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2021-01-31 21:07:51 +0300

    Enum

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ const person: {
   age: number;
 } = {
 */
+/*
 const person : {
   name: string;
   age: number;
@@ -14,6 +15,22 @@ const person : {
   age: 30,
   hobbiew: ['Sports', 'Cooking'],
   role: [2, 'author'],
+};
+*/
+
+/*
+const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
+*/
+
+enum Role { ADMIN = 5, READ_ONLY, AUTHOR };
+
+const person = {
+  name: 'Maximilian',
+  age: 30,
+  hobbiew: ['Sports', 'Cooking'],
+  role: Role.ADMIN,
 };
 
 // person.role.push('admin');
@@ -29,4 +46,8 @@ console.log('%c custom log', 'color:red;', person);
 for (const hobby of person.hobbiew) {
   console.log('%c custom log', 'color:red;', hobby.toUpperCase());
   // console.log('%c custom log', 'color:red;', hobby.map());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log('%c custom log', 'color:red;', 'adimn');
 }
