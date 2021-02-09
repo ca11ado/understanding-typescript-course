e60c1b5cb917ac5a380ec65aeae568959d929c05
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2021-02-09 22:36:06 +0300

    Before strict Compilation

---
+++ src/app.js
@@ -0,0 +1,6 @@
+"use strict";
+var button = document.querySelector('button');
+button.addEventListener('click', function () {
+    console.log('%c custom log', 'color:red;', 'button click');
+});
+//# sourceMappingURL=app.js.map
