9543c7aed5210ddd4ee7eda2588fa0c74f54d597
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2021-02-09 22:47:26 +0300

    Strict options

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,9 @@
 "use strict";
 var button = document.querySelector('button');
+function clickHandler(message) {
+    console.log('%c custom log', 'color:red;', 'click', message);
+}
 if (button) {
-    button.addEventListener('click', function () {
-        console.log('%c custom log', 'color:red;', 'button click');
-    });
+    button.addEventListener('click', clickHandler.bind(null, 'you r welcome'));
 }
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 const button = document.querySelector('button');
 
+function clickHandler(message: string) {
+  console.log('%c custom log', 'color:red;', 'click', message);
+}
+
 if (button) {
-  button.addEventListener('click', () => {
-    console.log('%c custom log', 'color:red;', 'button click');
-  });
+  button.addEventListener('click', clickHandler.bind(null, 'you r welcome'));
 }
