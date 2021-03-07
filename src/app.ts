8e2f3cbc709f5dc9aa202751c6c13f799b71c820
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2021-03-07 20:22:16 +0300

    Generic types

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,23 @@
-interface Bird {
-  type: 'bird';
-  flyingSpeed: number;
+const randomAr: Array<string> = ['test'];
+console.log(randomAr[0].length);
+
+const promise: Promise<number> = new Promise((resolve) => {
+  // reject('test');
+  resolve(100);
+});
+
+promise
+  .then(response => console.log(Number.isFinite(response)))
+  .catch(response => console.log(Number.isFinite(response)));
+
+function addRole<T extends object, U extends string>(user: T, role: U) {
+  return { ...user, role };
 }
 
-interface Dog {
-  type: 'dog';
-  runngingSpeed: number;
-}
+const user: object = {
+  name: 't0s',
+};
 
-type Animal = Bird | Dog;
+const role = 'admin';
 
-function movingAnimal(animal: Animal) {
-  switch(animal.type) {
-    case 'bird':
-      return animal.flyingSpeed;
-    case 'dog':
-      return animal.runngingSpeed;
-  }
-}
+console.log(addRole(user, role).role);
