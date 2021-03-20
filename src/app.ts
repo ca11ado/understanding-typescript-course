4375de6cea4c437777744f76a46a9a61e938fb53
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2021-03-20 15:39:00 +0300

    Decorators

--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,14 @@
-const randomAr: Array<string> = ['test'];
-console.log(randomAr[0].length);
-
-const promise: Promise<number> = new Promise((resolve) => {
-  // reject('test');
-  resolve(100);
-});
-
-promise
-  .then(response => console.log(Number.isFinite(response)))
-  .catch(response => console.log(Number.isFinite(response)));
-
-function addRole<T extends object, U extends string>(user: T, role: U) {
-  return { ...user, role };
+function PersonDecorator(constructor: Function) {
+  console.log('%c custom log', 'color:red;', 'decorator', constructor);
 }
 
-const user: object = {
-  name: 't0s',
-};
-
-const role = 'admin';
-
-console.log(addRole(user, role).role);
-
-interface Lengthy {
-  length: number;
-}
-
-function countAndPrint<T extends Lengthy>(element: T): [T, string] {
-  const length = element.length;
-  const description = length > 1
-    ? `Got ${length} elements`
-    : length === 0 ? 'Empty' : 'Got 1 element';
-  
-  return [element, description];
-}
-
-console.log(countAndPrint('Hi there'));
-console.log(countAndPrint(['test']));
-
-class DataStorage<T> {
-  private data: Array<T> = [];
-
-  addItem(item: T) {
-    this.data.push(item);
-  }
-
-  removeItem(item: T) {
-    this.data.splice(this.data.indexOf(item), 1);
-  }
-
-  getItems() {
-    return [...this.data];
+@PersonDecorator
+class Person {
+  name = 'Max';
+  constructor() {
+    console.log('%c custom log', 'color:red;', 'Creating person...');
   }
 }
 
-const numberStorage = new DataStorage<string>();
-numberStorage.addItem('111');
-numberStorage.removeItem('111');
-console.log('data storage');
-console.log(numberStorage.getItems());
+const person = new Person();
+console.log('%c custom log', 'color:red;', person);
