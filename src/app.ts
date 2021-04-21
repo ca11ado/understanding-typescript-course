d73e9a36c56bc2919af3e7cdb2d0e5348bb62a39
Khimtsov Anton Igorevich <projects.tlt@ya.ru>
2021-04-21 09:41:09 +0300

    Decorators done

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,59 @@
+const storedValidators: any = {
+
+};
+
 function PersonDecorator(constructor: Function) {
   console.log('%c custom log', 'color:red;', 'decorator', constructor);
 }
 
+function NameValidation(target: any, propName: string) {
+  console.log('%c custom log', 'color:red;', 'NameValidation', target, propName);
+  const constructorName = target.constructor.name;
+  const existingValidators = storedValidators[constructorName] || [];
+  storedValidators[constructorName] = [
+    ...existingValidators,
+    (instance: any) => instance[propName].length > 0,
+  ];
+}
+
+function KirillValidation(target: any, propName: string) {
+  const constructorName = target.constructor.name;
+  const existingValidators = storedValidators[constructorName] || [];
+  storedValidators[constructorName] = [
+    ...existingValidators,
+    (instance: any) => instance[propName] !== 'Kirill',
+  ];
+}
+
+function validate(instance: any): boolean {
+  console.log('%c custom log', 'color:red;', 'validate');
+  console.log('%c custom log', 'color:red;', instance, instance.constructor.name);
+  console.log('%c custom log', 'color:red;', storedValidators);
+  if (storedValidators[instance.constructor.name]) {
+    const validated = storedValidators[instance.constructor.name]
+      .map((validator: Function) => validator(instance));
+      console.log('%c custom log', 'color:red;', 'validated', validated);
+    return validated.every(Boolean);
+  }
+
+  return true;
+}
+
 @PersonDecorator
 class Person {
-  name = 'Max';
-  constructor() {
+  n: String;
+  constructor(n: String) {
+    this.n = n;
     console.log('%c custom log', 'color:red;', 'Creating person...');
+  }
+
+  @KirillValidation
+  @NameValidation
+  get name() {
+    return this.n;
   }
 }
 
-const person = new Person();
+const person = new Person('Kirill');
+validate(person);
 console.log('%c custom log', 'color:red;', person);
